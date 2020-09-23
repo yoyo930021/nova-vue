12ed90b9090cf1695d4bf46bb3fa0740499f0221
Tommaso Negri <tommasongr@icloud.com>
2020-09-23 15:21:31 +0000

    FEATURE: Add VLS correctly (waiting for better nova support for merge)

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -2,12 +2,89 @@ import './registrars/vueRegistrar.js'
 // require('./registrars/vueTemplateRegistrar.js');
 // require('./registrars/vueScriptRegistrar.js');
 
+let client
+
+// async function reload() {
+//     deactivate()
+//     console.log('Reloading...')
+//     await asyncActivate()
+// }
+
+async function installWrappedDependencies() {
+    return new Promise((resolve, reject) => {
+        const process = new Process('/usr/bin/env', {
+            args: ['npm', 'install'],
+            cwd: nova.extension.path,
+            stdio: ['ignore', 'pipe', 'pipe'],
+            env: {
+                NO_UPDATE_NOTIFIER: 'true',
+            },
+        })
+        if (nova.inDevMode()) {
+            process.onStdout((o) => console.log('installing:', o.trimRight()))
+        }
+        process.onStderr((e) => console.warn('installing:', e.trimRight()))
+        process.onDidExit((status) => {
+            if (status === 0) {
+                resolve()
+            } else {
+                reject(new Error('failed to install'))
+            }
+        })
+        process.start()
+    })
+}
+
+async function asyncActivate() {
+    await installWrappedDependencies()
+
+    try {
+        await installWrappedDependencies()
+    } catch (err) {
+        console.error('Failed to install')
+        throw err
+    }
+
+    const pathToVls = nova.path.join(
+        nova.extension.path,
+        'node_modules/vls/bin/vls'
+    )
+    const serviceArgs = {
+        path: pathToVls,
+    }
+
+    client = new LanguageClient(
+        'tommasonegri.vue',
+        'Vue Language Server',
+        {
+            type: 'stdio',
+            ...serviceArgs,
+            env: {
+                WORKSPACE_DIR: nova.workspace.path ? nova.workspace.path : '',
+            },
+        },
+        {
+            syntaxes: ['vue'],
+        }
+    )
+
+    client.start()
+}
+
 export function activate() {
-    // Do work when the extension is activated
-    console.log('Hello from Vue!')
+    console.log('Activating...')
+    return asyncActivate()
+        .catch((err) => {
+            console.error('Failed to activate')
+            console.error(err)
+        })
+        .then(() => {
+            console.log('Hello from Vue!')
+        })
 }
 
 export function deactivate() {
+    client.stop()
     // Clean up state before the extension is deactivated
     console.log('Goodbye!')
 }
